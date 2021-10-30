6c2693fd0b0853ff7efda6dcf8c1ddd67837a1a4
han.tang <1150137581@qq.com>
2021-10-30 08:01:19 +0000

    spawn start

--- tools/dataset/test_dataloader.py
+++ tools/dataset/test_dataloader.py
@@ -6,34 +6,45 @@ import argparse
 import torch
 import torch.distributed as dist
 
-def init_dist(rank, world_size):
+import torch.multiprocessing as mp
+
+def run(rank, size):
+    print(rank, size)
+
+def init_process(rank, world_size, fn):
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12345"
     dist.init_process_group(backend='nccl',
-		init_method="tcp://127.0.0.1:12345", rank=rank, world_size=world_size)
+		rank=rank, world_size=world_size)
+    fn(rank, world_size)
+
 
 def main(args):
+    size = 2
     data_dir = args.data_dir
+    '''
     local_rank = args.local_rank
     world_size = args.world_size
-    #init_dist(local_rank, world_size)
+    init_dist(local_rank, world_size)
     print(torch.distributed.is_nccl_available())
     '''
-    try:
-        world_size = int(os.environ['WORLD_SIZE'])
-        rank = int(os.environ['RANK'])
-        dist.init_process_group('nccl')
-    except KeyError:
-        world_size = 1
-        rank = 0
-        dist.init_process_group(backend='nccl', init_method="tcp://127.0.0.1:12584", rank=rank, world_size=world_size)
-    '''
+    processes = []
+    mp.set_start_method("spawn")
+    for rank in range(size):
+        p = mp.Process(target=init_process, args=(rank, size, run))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="")
     parser.add_argument("--data_dir", type=str, default="/home/users/han.tang/data/baseline_2030_V0.2/baseline_2030_V0.2.rec", help="")
-    parser.add_argument("--local_rank", type=int, default=0, help="")
-    parser.add_argument("--world_size", type=int, default=1, help="")
+    #parser.add_argument("--local_rank", type=int, default=0, help="")
+    #parser.add_argument("--world_size", type=int, default=1, help="")
     args= parser.parse_args()
     main(args)
 
